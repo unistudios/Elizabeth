5d0f6d6d046a80778754b7c70e171f287b0e49d0
root <root@usushifldv063.nbcuni.ge.com>
2011-06-13 11:04:58 -0700

    Upgrading from Django 1.1 to Django 1.3.  Brought in new settings.py, manage.py, and urls.py.

--- urls.py
+++ urls.py
@@ -1,48 +1,24 @@
-# from django.conf.urls.defaults import *
-# from django.views.generic.simple      import direct_to_template
-# import settings
-
-#Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-# urlpatterns = patterns('',
-    #main index goes to opsware for now.
-    # ('^$', 'opsware.views.index'),
-    
-    # (r'^opsware/', include('opsware.urls')),
-
-    # (r'^admin/', include(admin.site.urls)),
-    
-# )
-
-# if settings.DEBUG:
-        # urlpatterns += patterns('',
-                # (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'media'}),
-# )    
-
-from django.conf.urls.defaults import *
-from django.conf import settings
-from django.views.generic.simple import *
+from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-#        (r'^$', 'django.views.generic.simple.redirect_to', {'url' : 'http://wiki.nbcuni.ge.com/display/big/Unix-Linux+SOX+Compliancy'} ),
-        (r'^$', 'automation.views.home'), 
-        (r'^admin/(.*)', admin.site.root) ,
-        (r'^likewise/', include('likewise.urls')),
-        (r'^wikiexport/', include('wikiexport.urls')),
-        (r'^opsware/', include('opsware.urls')),
-        (r'^ice/', include('ice.urls')),
-        (r'^cacti/', 'cacti.views.cacti'),
-)
+    # Examples:
+    # url(r'^$', 'test1_3.views.home', name='home'),
+    # url(r'^test1_3/', include('test1_3.foo.urls')),
 
-# help us serve media for CherryPy server
-if settings.MEDIA_LOCAL:
-       urlpatterns += patterns('',
-               (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'media'}),
-)
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #(r'^$', 'automation.views.home'),
 
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+    (r'^likewise/', include('likewise.urls')),
+    #(r'^wikiexport/', include('wikiexport.urls')),
+    #(r'^opsware/', include('opsware.urls')),
+    #(r'^ice/', include('ice.urls')),
+    #(r'^cacti/', 'cacti.views.cacti'),
+)
