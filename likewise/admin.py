34595684fe7e7cf7beb731dc4dbab2c0f4cb0148
Kyle Flavin <Kyle.Flavin@nbcuni.com>
2011-06-23 11:53:24 -0700

    Modified the admin unixhost list display page slightly.

--- likewise/admin.py
+++ likewise/admin.py
@@ -9,7 +9,7 @@ class hostsettingInline(admin.TabularInline):
     #model = unixhost
 
 class unixhostAdmin(admin.ModelAdmin):
-    list_display = ('name', 'fqdn', 'level', hostsetting, 'os', 'id' )
+    list_display = ('name', 'level', 'os', 'id' )
     fields = ('name', 'fqdn', 'apps','level', 'os', 'comment')
     #inlines = [ hostsettingInline,]
     search_fields = ['name', 'fqdn']
