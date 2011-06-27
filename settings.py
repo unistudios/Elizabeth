b94bc592dc395c216de873b6ac7370001d6b0118
206102890 <206102890@NUUUNIITFLAVW7l.nbcuni.ge.com>
2011-06-27 10:48:42 -0700

    Added debug_toolbar.

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 # Django settings for upgrade project.
 
-
 #################################################################
 # Local settings that are set on a per box basis (dev vs. prod)
 # The settings_local.py file is not tracked in git.
@@ -122,3 +121,38 @@ LOGGING = {
         },
     }
 }
+
+
+try:
+    __import__("debug_toolbar")
+except ImportError:
+    pass
+else:
+    MIDDLEWARE_CLASSES += 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    INSTALLED_APPS += 'debug_toolbar',
+    INTERNAL_IPS = ('127.0.0.1',)
+    #TEMPLATE_DIRS += '/usr/lib/python2.6/dist-packages/debug_toolbar/tests/templates'
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+    )
+    def custom_show_toolbar(request):
+        return True # Always show toolbar, for example purposes only.
+    
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
+        #'EXTRA_SIGNALS': ['myproject.signals.MySignal'],
+        'HIDE_DJANGO_SQL': False,
+        'TAG': 'div',
+    }
+
+
+
