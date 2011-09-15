07982720cb618fdc272058fef8e6bb94bb71a719
Kyle Flavin <Kyle.Flavin@nbcuni.com>
2011-09-15 10:40:31 -0700

    fixed file uploading problems... hopefully

--- elizabeth/delivery.py
+++ elizabeth/delivery.py
@@ -4,6 +4,7 @@ from website.elizabeth.models import *
 from elizabeth.forms import *
 #from datetime import datetime as dt
 from django.http import HttpResponse
+from website.settings_local import *
 
 def uploadFile(request):
     if request.method == "POST":
@@ -17,15 +18,15 @@ def uploadFile(request):
 def handle_uploaded_file(f):
     #datetimenow=str(dt.now())
     #filename = datetimenow.split()[0] +  datetimenow.split()[1].split(".")[0].replace(":", "")
-       
+
     if str(f).endswith("py"):
         print "blaster/"
-        directory = "blaster/"
+        directory = "/elizabeth/blaster/"
     else:
         print "ogfs_output/"
-        directory = "ogfs_output/"
+        directory = "/elizabeth/ogfs_output/"
     
-    destination = open('elizabeth/' + directory + str(f), 'wb')
+    destination = open(ELIZ_PATH + directory + str(f), 'wb')
     for chunk in f.chunks():
         destination.write(chunk)
     destination.close()
