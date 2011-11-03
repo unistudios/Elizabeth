407ee10c1d894a2b2fd76224975de485a6cf560c
Kyle Flavin <Kyle.Flavin@nbcuni.com>
2011-11-03 14:21:38 -0700

    updated push_wiki.sh

--- elizabeth/blaster/push_wiki.sh
+++ elizabeth/blaster/push_wiki.sh
@@ -1,7 +1,18 @@
-if [ ! $DJANGOWEB ]
+unset http_proxy
+unset FTP_PROXY
+unset ftp_proxy
+unset HTTP_PROXY 
+
+IP=$(/sbin/ifconfig eth0 | grep "inet addr:" | awk '{ print $2 }' | cut -f2 -d":")
+
+if [ $IP -eq "3.156.190.164" ]
 then
- DJANGOWEB="http://3.156.190.164/wikiexport/"
-fi   
+ DJANGOWEB="http://3.156.190.164.nbcuni.ge.com/wikiexport/"
+else
+ DJANGOWEB="http://likewise.nbcuni.ge.com/wikiexport/"
+fi
+
+cd `pwd`"/elizabeth/blaster"
 webdavcmd="tmp/cadaver.cmd"
 urloutput="tmp/urloutput.txt"   # get the output from what we need to process.
 curl -s $DJANGOWEB > $urloutput 
