05fd15c507c6fcee223951dc7b20c1abcb4c3aa8
Kyle Flavin <Kyle.Flavin@nbcuni.com>
2011-11-03 14:36:36 -0700

    push_wiki.sh changes

--- elizabeth/blaster/push_wiki.sh
+++ elizabeth/blaster/push_wiki.sh
@@ -1,3 +1,7 @@
+
+
+ELIZ_DIR="./elizabeth/blaster"
+
 unset http_proxy
 unset FTP_PROXY
 unset ftp_proxy
@@ -5,14 +9,17 @@ unset HTTP_PROXY
 
 IP=$(/sbin/ifconfig eth0 | grep "inet addr:" | awk '{ print $2 }' | cut -f2 -d":")
 
-if [ $IP -eq "3.156.190.164" ]
+if [ $IP == "3.156.190.164" ]
 then
- DJANGOWEB="http://3.156.190.164.nbcuni.ge.com/wikiexport/"
+ DJANGOWEB="http://3.156.190.164/wikiexport/"
 else
  DJANGOWEB="http://likewise.nbcuni.ge.com/wikiexport/"
 fi
 
-cd `pwd`"/elizabeth/blaster"
+if [ -d $ELIZ_DIR ]; then
+ cd $ELIZ_DIR
+fi
+
 webdavcmd="tmp/cadaver.cmd"
 urloutput="tmp/urloutput.txt"   # get the output from what we need to process.
 curl -s $DJANGOWEB > $urloutput 
